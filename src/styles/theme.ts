c26681ca697bfbf461dcac64581eedde3c30a91f
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2022-06-29 18:52:10 -0300

    chore: project setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    green: '#1BAFAC',
+    light_green: '#DBFFFF',
+
+    light_blue: '#1485C8',
+    dark_blue: '#043560',
+
+    orange: '#F06630',
+    yellow: '#FAAE31',
+
+    white: '#FFFFFF',
+  },
+};
